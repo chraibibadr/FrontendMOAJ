e795cb6bff9231433dbc1ea4a150777bed2b0f9c
BADER <badr.chraibibadr@gmail.com>
2022-09-20 16:15:15 +0100

    routes configuration

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,15 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/Home.vue') }],
+  },
+  {
+    path: '/login',
+    component: () => import('pages/common/Login.vue'),
+  },
+  {
+    path: '/menu',
+    component: () => import('pages/common/AppsMenu.vue'),
   },
 
   // Always leave this as last one,
