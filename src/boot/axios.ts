20ec6ae6e1a35a5251eef73bd789a6fbb0efbf3b
BADER <badr.chraibibadr@gmail.com>
2022-09-20 16:13:15 +0100

    axios jwt logic

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -3,6 +3,32 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 
+let refresh = false;
+
+axios.interceptors.response.use(
+  (resp) => resp,
+  async (error) => {
+    if (error.response.status === 401 && !refresh) {
+      refresh = true;
+      const { status, data } = await axios.post(
+        'auth/refresh',
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      if (status === 200) {
+        axios.defaults.headers.common['Authorization'] =
+          'Bearer ' + data.access_token;
+
+        return axios(error.config);
+      }
+    }
+    refresh = false;
+    return error;
+  }
+);
+
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
 });
