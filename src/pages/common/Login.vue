8f07b06d22d6657556e045d9d7f3a8ea651f158f
BADER <badr.chraibibadr@gmail.com>
2022-09-20 16:14:04 +0100

    adding login page with jwt

---
+++ src/pages/common/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="q-pa-md window-height">
+    <div class="row justify-center full-height items-center">
+      <div class="col-4">
+        <q-icon
+          class="text-center full-width"
+          size="5em"
+          color="primary"
+          name="yard"
+        />
+        <h6 class="text-center text-primary">
+          Se connecter avec votre compte.
+        </h6>
+        <q-form @submit.prevent="submit" class="q-gutter-md">
+          <q-input
+            filled
+            v-model="email"
+            name="email"
+            label="Adresse e-mail / nom d'utilisateur"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Requis']"
+          >
+            <template v-slot:prepend>
+              <q-icon name="person" />
+            </template>
+          </q-input>
+
+          <q-input
+            v-model="password"
+            name="password"
+            label="Mot de passe"
+            filled
+            :type="isPwd ? 'password' : 'text'"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Requis']"
+          >
+            <template v-slot:prepend>
+              <q-icon name="lock" />
+            </template>
+
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+
+          <div>
+            <q-btn
+              padding="md"
+              class="full-width"
+              label="SE CONNECTER"
+              type="submit"
+              color="primary"
+            />
+          </div>
+        </q-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+import { axios } from 'boot/axios';
+import { ref } from 'vue';
+
+export default {
+  name: 'Login-page',
+  setup() {
+    const email = ref(null);
+    const password = ref(null);
+    const router = useRouter();
+
+    const submit = async (e) => {
+      const form = new FormData(e.target);
+
+      const inputs = Object.fromEntries(form.entries());
+
+      const { data } = await axios.post('auth/login', inputs, {
+        withCredentials: true,
+      });
+
+      axios.defaults.headers.common['Authorization'] =
+        'Bearer ' + data.access_token;
+
+      await router.push('/menu');
+    };
+
+    return {
+      isPwd: ref(true),
+      submit,
+      email,
+      password,
+    };
+  },
+};
+</script>
