250d6f51062551ad02825d399e6041d02dabbcd9
BADER <badr.chraibibadr@gmail.com>
2022-09-23 18:01:14 +0100

    adding meta & name to routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,43 +3,63 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('pages/common/Login.vue'),
-  },
-  {
-    path: '/login',
-    component: () => import('pages/common/Login.vue'),
-  },
-  {
-    path: '/menu',
+    meta: {
+      isAuthenticated: true,
+    },
     component: () => import('pages/common/AppsMenu.vue'),
   },
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('pages/common/Login.vue'),
+  },
+  {
     path: '/demands',
+    meta: {
+      isAuthenticated: true,
+      App4: true,
+    },
     component: () => import('layouts/DemandLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/demand/DemandsList.vue') },
+      {
+        path: '',
+        name: 'demands',
+        component: () => import('pages/demand/DemandsList.vue'),
+      },
       {
         path: 'config',
+        name: 'demandConfig',
         component: () => import('pages/demand/DemandConfig.vue'),
       },
     ],
   },
   {
     path: '/property',
+    meta: {
+      isAuthenticated: true,
+      App3: true,
+    },
     component: () => import('layouts/PropertyLayout.vue'),
     children: [
       {
         path: '',
+        name: 'property',
         component: () => import('pages/property/PropertyList.vue'),
       },
       {
         path: 'config',
+        name: 'propertyConfig',
         component: () => import('pages/property/PropertyConfig.vue'),
       },
     ],
   },
   {
     path: '/test',
+    name: 'test',
+    meta: {
+      isAuthenticated: true,
+      isAdmin: true,
+    },
     component: () => import('pages/Test.vue'),
   },
 
